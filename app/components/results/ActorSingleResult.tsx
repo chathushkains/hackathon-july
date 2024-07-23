0f665f1504e9a8a32ad5855b469f48085e3ca73c
Chathushka <chathushka.p@insighture.com>
2024-07-23 18:53:59 +0530

    updates

--- app/components/results/ActorSingleResult.tsx
+++ app/components/results/ActorSingleResult.tsx
@@ -130,7 +130,7 @@ export default function ActorSingleResult(props: any) {
                                 <div key={property.id} className="group relative">
                                     <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                                         <img
-                                            src={property.images[0]}
+                                            src={property.image}
                                             className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                                         />
                                     </div>
