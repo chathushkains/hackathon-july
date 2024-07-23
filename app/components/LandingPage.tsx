846480f97b22e76db31abe6ab5a0b77094a47ca1
Chathushka <chathushka.p@insighture.com>
2024-07-23 23:33:46 +0530

    update

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -259,7 +259,7 @@ export default function LandingPage() {
                 <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:pt-56">
                     <div className="text-center">
                         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                            Hutly Genie
+                            Hutly Genie 🧞
                         </h1>
                         <p className="mt-8 text-lg leading-8 text-gray-60">
                             Please type in your wishes, one at a time.
